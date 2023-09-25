e0ae570008da06c65b128ae636f7619a942dbf8c
Bradly Chang <57744542+bradly0cjw@users.noreply.github.com>
2023-09-25 11:10:22 +0800

    [add] stack and queue implement
    stack and ring queue by using c++
    
    Signed-off-by: Bradly Chang <57744542+bradly0cjw@users.noreply.github.com>

---
+++ Week3/Prob4.cpp
@@ -0,0 +1,80 @@
+//
+// Created by LINBEI on 9/25/2023.
+//
+#include <iostream>
+#include <cstring>
+
+#define SIZE 4
+
+using namespace std;
+
+class Queue
+{
+public:
+    Queue()
+    {
+        top = 0;
+        bot = 0;
+    }
+    int enqueue(int element)
+    {
+        if ((top + 1) % SIZE == bot){
+            return -1;
+        }
+        data[top]=element;
+        top=(top + 1) % SIZE;
+        return 1;
+    }
+    int *dequeue()
+    {
+        if (top==bot){
+            return NULL;
+        }
+        int *temp = &data[bot];
+        bot=(bot+1)%SIZE;
+        return temp;
+    }
+private:
+    int data[SIZE];
+    int top, bot;
+};
+
+int main()
+{
+    int data, *temp;
+    char command[50];
+    Queue *queue = new Queue();
+    while(1)
+    {
+        cin>>command;
+        if(strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+        else if(strcmp(command, "enqueue") == 0)
+        {
+            cout<<"Please input a integer data:";
+            cin>>data;
+            if(queue->enqueue(data) == 1)
+            {
+                cout<<"Successfully enqueue data "<<data<<" into queue."<<endl;
+            }
+            else
+            {
+                cout<<"Failed to enqueue data into queue."<<endl;
+            }
+        }
+        else if(strcmp(command, "dequeue") == 0)
+        {
+            temp = queue->dequeue();
+            if(temp == NULL)
+            {
+                cout<<"Failed to dequeue a data from queue.\n";
+            }
+            else
+            {
+                cout<<"Dequeue data "<<*temp<<" from queue."<<endl;
+            }
+        }
+    }
+}
